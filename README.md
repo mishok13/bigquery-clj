e92d77ef95089c58d4270084a2206278d5655566
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-12-10 11:24:22 +0100

    Adding coveralls support

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![License MIT][badge-license]](http://opensource.org/licenses/MIT)
 [![Build Status](https://travis-ci.org/screen6/bigquery-clj.png?branch=master)](https://travis-ci.org/screen6/bigquery-clj)
+[![Coverage Status](https://img.shields.io/coveralls/screen6/cardinality.svg)](https://coveralls.io/r/screen6/bigquery-clj)
 
 # io.screen6/bigquery
 
--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+                   :plugins [[lein-midje "3.1.3"]
+                             [lein-cloverage "1.0.2"]]}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.google.apis/google-api-services-bigquery "v2-rev175-1.19.0"]
                  [org.clojure/tools.logging "0.3.1"]])
