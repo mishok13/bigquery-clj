d4aac5fbccd249a2e3457c1ebd4e89e091d5ab80
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-12-10 14:04:56 +0100

    Automatically push jar to Clojars repo

---
+++ bin/build/clojars.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Deploy the SNAPSHOT version to Clojars. Additional check is needed
+# to ensure that we don't deploy release versions accidentally (we
+# shouldn't have them in master first of all, but that's a whole
+# different story)
+
+lein2 deploy clojars
