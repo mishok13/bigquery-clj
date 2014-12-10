eb4bd86f65cefa225c36253ae25e755415f86e04
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-12-10 14:36:45 +0100

    Only deploy to clojars from master branch

--- bin/build/clojars.sh
+++ bin/build/clojars.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+set -e
+set -x
+
 # Deploy the SNAPSHOT version to Clojars. Additional check is needed
 # to ensure that we don't deploy release versions accidentally (we
 # shouldn't have them in master first of all, but that's a whole
 # different story)
 
+# Check that we're not building PR and we're on master branch
+test $TRAVIS_PULL_REQUEST == "false"
+test $TRAVIS_BRANCH == "master"
+
+# Deploy a snapshot
 lein2 deploy snapshots
