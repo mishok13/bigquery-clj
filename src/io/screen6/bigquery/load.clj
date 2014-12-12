16e6eb8ddc543aa42a75a73c3f86d5b379254c32
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2014-12-12 11:54:26 +0100

    One succesfull upload. That's something, I guess.

--- src/io/screen6/bigquery/load.clj
+++ src/io/screen6/bigquery/load.clj
@@ -1,8 +1,9 @@
 (ns io.screen6.bigquery.load
   "Loading data into BigQuery"
   (:require
+   [cheshire.core :as json]
    [clj-http.client :as http]
-   [cheshire.core :as json]))
+   [clojure.java.io :as io]))
 
 (defn schema
   [project dataset table columns]
@@ -14,19 +15,33 @@
                         :datasetId dataset
                         :tableId table}}}})
 
+(def ^:private RESUMABLE-UPLOAD-URL
+  "https://www.googleapis.com/upload/bigquery/v2/projects/%s/jobs?uploadType=resumable")
+
 (defn multipart
-  "Multipart upload"
-  [bq project-id path]
+  "Multipart upload
+
+  The steps are:
+  1. Get resumable session id
+  2. Upload file
+  3. Handle failures"
+  [bq project dataset table columns path]
+  ;; Just get a new token every time, that's the stupidest and yet
+  ;; most effective way to handle this for now
   (.refreshToken bq)
-  (let [token (.getAccessToken bq)
-        url (format "https://www.googleapis.com/upload/bigquery/v2/projects/%s/jobs?uploadType=resumable" project-id)]
-    (http/post url
-               {:throw-entire-message? true
-                :debug true
-                :debug-body true
-                :body (json/encode (schema "dedup-bong-22" "uploadtest" "test" [["foo" "STRING"]]))
-                :headers {"X-Upload-Content-Type" "application/octet-stream"
-                          "X-Upload-Content-Length" "2000000"
-                          "Content-Type" "application/json; charset=UTF-8"
-                          ;; "Content-Length" "0"
-                          "Authorization" (format "Bearer %s" token)}})))
+  (let [content-length (.length (io/file path))
+        session-url (get-in (http/post (format RESUMABLE-UPLOAD-URL project)
+                                       {:body (json/encode (schema project dataset table columns))
+                                        :headers {"X-Upload-Content-Type" "application/octet-stream"
+                                                  "X-Upload-Content-Length" content-length
+                                                  "Content-Type" "application/json; charset=UTF-8"
+                                                  "Authorization" (format "Bearer %s" (.getAccessToken bq))}}) [:headers "Location"])]
+    (http/put session-url
+              {:body (slurp path)
+               :headers {"Authorization" (format "Bearer %s" (.getAccessToken bq))
+                         "Content-Type" "application/octet-stream"}})))
+
+(defn jobs
+  [bq project]
+  (http/get (format "https://www.googleapis.com/bigquery/v2/projects/%s/jobs" project)
+            {:headers {"Authorization" (format "Bearer %s" (.getAccessToken bq))}}))
